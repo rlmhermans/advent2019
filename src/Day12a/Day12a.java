03a91a19678299fa9c4c834235362afe8eb4f7b8
Ruud Hermans <rlmhermans@gmail.com>
2019-12-12 11:14:21 +0100

    Day 12a finished

---
+++ src/Day12a/Day12a.java
@@ -0,0 +1,45 @@
+package Day12a;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day12a {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner fileReader = new Scanner(new File("src/Day12a/MoonInput.txt"));
+        ArrayList<Moon> moons = new ArrayList<Moon>();
+        while(fileReader.hasNextLine()) {
+            String line = fileReader.nextLine();
+            line = line. substring(1, line.length()-1);
+            String[] positions = line.split(", ");
+            int x = Integer.parseInt(positions[0].substring(2));
+            int y = Integer.parseInt(positions[1].substring(2));
+            int z = Integer.parseInt(positions[2].substring(2));
+            moons.add(new Moon(x, y, z));
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            for(Moon current : moons) {
+                for(Moon other : moons) {
+                    if(current != other) {
+                        current.processGravityInfluenceBy(other);
+                    }
+                }
+            }
+
+            for(Moon current : moons) {
+                current.move();
+            }
+        }
+
+        int total = 0;
+
+        for(Moon m : moons) {
+            total += m.getTotalEnergy();
+        }
+
+        System.out.println(total);
+    }
+}
---
+++ src/Day12a/Moon.java
@@ -0,0 +1,41 @@
+package Day12a;
+
+import java.math.BigInteger;
+
+public class Moon {
+    private int positionX;
+    private int positionY;
+    private int positionZ;
+
+    private int velocityX;
+    private int velocityY;
+    private int velocityZ;
+
+    public Moon(int positionX, int positionY, int positionZ) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.positionZ = positionZ;
+    }
+
+    public void move() {
+        positionX += velocityX;
+        positionY += velocityY;
+        positionZ += velocityZ;
+    }
+
+    public void processGravityInfluenceBy(Moon other) {
+        if(this.positionX < other.positionX) velocityX++;
+        if(this.positionX > other.positionX) velocityX--;
+        if(this.positionY < other.positionY) velocityY++;
+        if(this.positionY > other.positionY) velocityY--;
+        if(this.positionZ < other.positionZ) velocityZ++;
+        if(this.positionZ > other.positionZ) velocityZ--;
+    }
+
+    public int getTotalEnergy() {
+        int pot = Math.abs(positionX) + Math.abs(positionY) + Math.abs(positionZ);
+        int kin = Math.abs(velocityX) + Math.abs(velocityY) + Math.abs(velocityZ);
+
+        return pot * kin;
+    }
+}
