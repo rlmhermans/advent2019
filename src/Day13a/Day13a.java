1a8b2294414427f7e8623458ece8a1c5e1b77710
Ruud Hermans <rlmhermans@gmail.com>
2019-12-13 18:59:37 +0100

    Day 13 part 1 finished

---
+++ src/Day13a/Day13a.java
@@ -0,0 +1,7 @@
+package Day13a;
+
+public class Day13a {
+    public static void main(String[] args) {
+        new Arcade().run();
+    }
+}
---
+++ src/Day13b/Day13b.java
@@ -0,0 +1,7 @@
+package Day13b;
+
+public class Day13b {
+    public static void main(String[] args) {
+        new Arcade().run();
+    }
+}
