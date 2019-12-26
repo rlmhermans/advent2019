25813d9cccb03f3aa54a716121309fb151beb3d4
Ruud Hermans <rlmhermans@gmail.com>
2019-12-26 12:39:14 +0100

    Day 19b finished

--- src/Day19b/Droid.java
+++ src/Day19b/Droid.java
@@ -1,13 +1,10 @@
-package Day19a;
+package Day19b;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Droid {
-    int currentInput = 0;
-    int amount = 0;
-    ArrayList<Integer> points = new ArrayList<>();
-    int size = 1100;
-    char[][] scanField = new char[size][size];
+    LinkedList<Integer> coors = new LinkedList<>();
+    int output = 0;
     private static final long[] program = {109, 424, 203, 1, 21101, 11, 0, 0, 1105, 1, 282, 21101, 18, 0, 0, 1106, 0, 259, 2102, 1, 1, 221, 203, 1, 21101, 0, 31, 0, 1106, 0, 282, 21102, 38, 1, 0, 1105, 1, 259, 20101, 0, 23, 2, 22101, 0, 1, 3, 21101, 1, 0, 1, 21101, 0, 57, 0, 1105, 1, 303, 2101, 0, 1, 222, 20102, 1, 221, 3, 21001, 221, 0, 2, 21101, 0, 259, 1, 21101, 80, 0, 0, 1105, 1, 225, 21101, 137, 0, 2, 21101, 91, 0, 0, 1105, 1, 303, 1202, 1, 1, 223, 21001, 222, 0, 4, 21102, 259, 1, 3, 21101, 225, 0, 2, 21102, 225, 1, 1, 21101, 0, 118, 0, 1106, 0, 225, 20102, 1, 222, 3, 21101, 0, 88, 2, 21102, 133, 1, 0, 1105, 1, 303, 21202, 1, -1, 1, 22001, 223, 1, 1, 21101, 0, 148, 0, 1106, 0, 259, 1202, 1, 1, 223, 20102, 1, 221, 4, 20101, 0, 222, 3, 21101, 24, 0, 2, 1001, 132, -2, 224, 1002, 224, 2, 224, 1001, 224, 3, 224, 1002, 132, -1, 132, 1, 224, 132, 224, 21001, 224, 1, 1, 21102, 1, 195, 0, 106, 0, 108, 20207, 1, 223, 2, 20102, 1, 23, 1, 21101, -1, 0, 3, 21101, 0, 214, 0, 1105, 1, 303, 22101, 1, 1, 1, 204, 1, 99, 0, 0, 0, 0, 109, 5, 2102, 1, -4, 249, 22102, 1, -3, 1, 22102, 1, -2, 2, 22102, 1, -1, 3, 21101, 0, 250, 0, 1105, 1, 225, 22102, 1, 1, -4, 109, -5, 2106, 0, 0, 109, 3, 22107, 0, -2, -1, 21202, -1, 2, -1, 21201, -1, -1, -1, 22202, -1, -2, -2, 109, -3, 2106, 0, 0, 109, 3, 21207, -2, 0, -1, 1206, -1, 294, 104, 0, 99, 22101, 0, -2, -2, 109, -3, 2105, 1, 0, 109, 5, 22207, -3, -4, -1, 1206, -1, 346, 22201, -4, -3, -4, 21202, -3, -1, -1, 22201, -4, -1, 2, 21202, 2, -1, -1, 22201, -4, -1, 1, 22102, 1, -2, 3, 21102, 343, 1, 0, 1105, 1, 303, 1105, 1, 415, 22207, -2, -3, -1, 1206, -1, 387, 22201, -3, -2, -3, 21202, -2, -1, -1, 22201, -3, -1, 3, 21202, 3, -1, -1, 22201, -3, -1, 2, 21202, -4, 1, 1, 21102, 1, 384, 0, 1106, 0, 303, 1106, 0, 415, 21202, -4, -1, -4, 22201, -4, -3, -4, 22202, -3, -2, -2, 22202, -2, -4, -4, 22202, -3, -2, -3, 21202, -4, -1, -2, 22201, -3, -2, 1, 22101, 0, 1, -4, 109, -5, 2106, 0, 0};
 
     public static void main(String[] args) {
@@ -15,37 +12,39 @@ public class Droid {
     }
 
     public void run() {
-        for (int y = 0; y < size; y++) {
-            for (int x = 0; x < size; x++) {
-                points.add(x);
-                points.add(y);
-                scanField[y][x] = '.';
-            }
-        }
+        boolean found = false;
+        int startY = 1000;
 
-        while (currentInput < points.size()) {
+        while (!found) {
+            boolean lineStartFound = false;
+            int startX = startY * 10 / 14;
+
+            while (!lineStartFound) {
+                startX++;
+                coors.addLast(startX);
+                coors.addLast(startY);
+                new VM(program, this).runProgram();
+                lineStartFound = output == 1;
+            }
+
+            coors.addLast(startX + 99);
+            coors.add(startY - 99);
             new VM(program, this).runProgram();
-        }
 
-        System.out.println(amount);
-        for (int y = 0; y < size; y++) {
-            for (int x = 0; x < size; x++) {
-                System.out.print(scanField[y][x]);
+            if (output == 1) {
+                System.out.println("Start point at " + startX + ":" + (startY - 99));
+                found = true;
+            } else {
+                startY++;
             }
-            System.out.println();
         }
     }
 
     public int input() {
-        int value = points.get(currentInput);
-        currentInput++;
-        return value;
+        return coors.pop();
     }
 
     public void output(int value) {
-        if (value == 1) {
-            amount++;
-            scanField[points.get(currentInput-1)][points.get(currentInput-2)] = '#';
-        }
+        output = value;
     }
 }
