d86711aeb013c57d573b0014e44d3fc37834dcb8
Ruud Hermans <rlmhermans@gmail.com>
2019-12-26 09:27:13 +0100

    Day 19a finished

--- src/Day18a.java
+++ src/Day18a.java
@@ -1,11 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Day18a {
-    boolean[][] visited;
-    char[][] maze;
-
     public static void main(String[] args) {
         new Day18a().run();
     }
@@ -18,129 +17,112 @@ public class Day18a {
             e.printStackTrace();
         }
 
-        int sizeX = 81;
-        int sizeY = 81;
-        maze = new char[sizeY][sizeX];
-        int startX = 0;
-        int startY = 0;
-        int keys = 0;
+        ArrayList<String> fileContents = new ArrayList<>();
+        while (fileReader.hasNextLine()) {
+            fileContents.add(fileReader.nextLine());
+        }
+
+        int sizeX = fileContents.get(0).length();
+        int sizeY = fileContents.size();
+        char[][] maze = new char[sizeY][sizeX];
 
         int lineNumber = 0;
-        while (fileReader.hasNextLine()) {
-            String line = fileReader.nextLine();
-            for (int i = 0; i < line.length(); i++) {
-                maze[lineNumber][i] = line.charAt(i);
-                if (String.valueOf(line.charAt(i)).matches("[a-z]")) keys++;
 
-                if (line.charAt(i) == '@') {
-                    startX = i;
-                    startY = lineNumber;
-                }
+        Point middle = null;
+
+        for (String line : fileContents) {
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                maze[lineNumber][i] = c;
+                if (c == '@') middle = new Point(i, lineNumber, 0);
             }
+
             lineNumber++;
         }
 
-        LinkedList<Node> foundNodeList = new LinkedList<>();
-        LinkedList<Node> checkedNodeList = new LinkedList<>();
-        ArrayList<Node> nodes = new ArrayList<>();
+        int[] xEntrance = {-2, 2, -1, 1, -2, 2, -1, 1};
+        int[] yEntrance = {-1, -1, -2, -2, 1, 1, 2, 2};
+        ArrayList<Path> paths = new ArrayList<>();
 
-        Node startNode = new Node(startX, startY, 0);
-        nodes.add(startNode);
-        foundNodeList.add(startNode);
+        for (int i = 0; i < xEntrance.length; i++) {
+            paths.add(new Path(middle.x + xEntrance[i], middle.y + yEntrance[i]));
+        }
 
-        int shortest = Integer.MAX_VALUE;
-        while (!foundNodeList.isEmpty()) {
-            Node node = foundNodeList.pop();
-            checkedNodeList.addLast(node);
-            boolean done = node.foundKeys.length() == keys;
+        for (Path path : paths) {
+            boolean[][] visited = new boolean[sizeY][sizeX];
+            LinkedList<Point> points = new LinkedList<>();
+            int[] xDirections = {-1, 1, 0, 0};
+            int[] yDirections = {0, 0, -1, 1};
 
-            if (done) {
-                if (shortest > node.point.distanceFromStart) shortest = node.point.distanceFromStart;
-            } else {
-                visited = new boolean[sizeY][sizeX];
-                LinkedList<Point> points = new LinkedList<>();
-                int[] xDirections = {-1, 1, 0, 0};
-                int[] yDirections = {0, 0, -1, 1};
+            points.add(path.start);
 
-                points.add(node.point);
-                boolean longerPath = false;
-                while (!points.isEmpty() && !longerPath) {
-                    Point p = points.pop();
+            while (!points.isEmpty()) {
+                Point p = points.pop();
+                visited[p.y][p.x] = true;
+                char field = maze[p.y][p.x];
 
-                    if (p.distanceFromStart >= shortest) longerPath = true;
+                if (Character.isLetter(field)) {
+                    p.name = field;
+                    path.keys.add(p);
+                }
 
-                    for (int i = 0; i < 4; i++) {
-                        Point next = new Point(p.x + xDirections[i], p.y + yDirections[i], p.distanceFromStart + 1);
-                        if (allowedMove(node, next.x, next.y)) {
-                            visited[next.y][next.x] = true;
-                            String field = String.valueOf(maze[next.y][next.x]);
-
-                            boolean found = false;
-                            if (field.matches("[a-z]")) {
-                                if (!node.foundKeys.contains(field)) {
-                                    Node newNode = new Node(next.x, next.y, next.distanceFromStart);
-                                    newNode.foundKeys = node.foundKeys + field;
-                                    foundNodeList.addLast(newNode);
-                                    nodes.add(newNode);
-                                    newNode.previous = node;
-                                    found = true;
-                                }
-                            }
-
-                            if (!found) points.addLast(next);
-                        }
+                for (int i = 0; i < 4; i++) {
+                    Point next = new Point(p.x + xDirections[i], p.y + yDirections[i], p.distanceFromStart + 1);
+                    if (!visited[next.y][next.x] && maze[next.y][next.x] != '#') {
+                        points.addLast(next);
                     }
                 }
             }
         }
 
-        ArrayList<Node> candidates = new ArrayList<>();
-        for (Node n : nodes) {
-            if (n.foundKeys.length() == keys) candidates.add(n);
-        }
+        paths.removeIf(x -> x.keys.size() < 2);
 
-        int least = Integer.MAX_VALUE;
-        for (Node n : candidates) {
-            int length = n.point.distanceFromStart;
-            if (length < least) least = length;
-        }
+        for (int i = 0; i < paths.size(); i++) {
+            Path p = paths.get(i);
+            System.out.println(String.format("Path %d: %s", i, p.keys));
 
-        System.out.println(least);
+            for (int j = 0; j < paths.size(); j++) {
+                if(i != j) System.out.println(String.format("Path %d to path %d: %d", i, j, p.distanceTo(paths.get(j))));
+            }
 
-    }
-
-    private boolean allowedMove(Node node, int x, int y) {
-        if (visited[y][x]) return false;
-
-        String nextTile = String.valueOf(maze[y][x]);
-        if (nextTile.matches("[a-z.@]")) return true;
-
-        if (nextTile.matches("[A-Z]")) {
-            return node.foundKeys.contains(nextTile.toLowerCase());
-        }
-
-        return false;
-    }
-
-    private class Node {
-        Node previous;
-        Point point;
-        String foundKeys = "";
-
-        public Node(int x, int y, int distance) {
-            this.point = new Point(x, y, distance);
+            Path middlePath = new Path(middle.x, middle.y);
+            System.out.println(String.format("Path %d to middle: %d", i,  p.distanceTo(middlePath)));
         }
     }
 
     private class Point {
-        private int x;
-        private int y;
-        private int distanceFromStart;
+        int x;
+        int y;
+        int distanceFromStart;
+        char name;
 
         public Point(int x, int y, int distanceFromStart) {
             this.x = x;
             this.y = y;
             this.distanceFromStart = distanceFromStart;
+        }
+
+        @Override
+        public String toString() {
+            return name + " (" + distanceFromStart + ")";
+        }
+    }
+
+    private class Path {
+        Point start;
+        ArrayList<Point> keys = new ArrayList<>();
+        int depth = 0;
+        boolean end = false;
+
+        public Path(int x, int y) {
+            start = new Point(x, y, 0);
+        }
+
+        public int distanceTo(Path other) {
+            int xDistance = Math.abs(start.x - other.start.x);
+            int yDistance = Math.abs(start.y - other.start.y);
+
+            return xDistance + yDistance;
         }
     }
 }
